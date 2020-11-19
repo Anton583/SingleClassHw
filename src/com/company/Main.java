0f2ffc2c84144908d84f8c042fd48040a22e53a7
Anton <anton.bartcev@gmail.com>
2020-11-19 13:15:41 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import ibcsutils.Book;
+
+public class Main {
+
+    public static void main(String[] args) {
+    hw3();
+    }
+    static void hw2(){
+        Book[] books = new Book[2];
+        String[] author1 = {"Fyodor Dostoevsky"};
+        String[] author2 = {"Alexander Pushkin"};
+        Book book1 = new Book("The Idiot", author1, 1869);
+        Book book2 = new Book("The Bronze Horseman", author2, 1837);
+        books[0] = book1;
+        books[1] = book2;
+        for(Book book : books)
+            System.out.println(book.toString());
+    }
+    static void hw3(){
+        Newspaper newspaper = new Newspaper("Newest Paper", 2020, 11, 18, true);
+        Newspaper newspaper1 = new Newspaper("Born to live", 2003, 6, 8, false);
+        Newspaper[] newspapers = {newspaper, newspaper1};
+        for(Newspaper np : newspapers){
+            System.out.println(np.toString());
+        }
+    }
+}
